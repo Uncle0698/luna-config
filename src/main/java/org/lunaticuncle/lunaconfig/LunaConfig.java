fc799020819f3e49c5a08dd26b26c852f6f2503a
Uncle0698 <kevin201560513@gmail.com>
2024-07-02 17:00:45 +0800

    Initial commit

---
+++ src/main/java/org/lunaticuncle/lunaconfig/LunaConfig.java
@@ -0,0 +1,26 @@
+package org.lunaticuncle.lunaconfig;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.util.GameStartEntrypoint;
+
+
+public class LunaConfig implements ModInitializer, GameStartEntrypoint{
+    public static final String MOD_ID = "lunaconfig";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Luna-Config initialized.");
+    }
+
+	@Override
+	public void beforeGameStart() {
+
+	}
+
+	@Override
+	public void afterGameStart() {
+
+	}
+}
